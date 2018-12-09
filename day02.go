c5e08c6759054f61f4a4c05e192f911293263350
Miriam Lauter <lauter.miriam@gmail.com>
2018-12-09 13:48:29 -0500

    Add day02 pt 2

--- day02.go
+++ day02.go
@@ -45,11 +45,85 @@ func checksum(f io.ReadSeeker) (int, error) {
 	return checksum, nil
 }
 
+// return bool: was exactly one letter different
+//         int: index at which a letter was different
+//       error
+func compareIds(a, b []rune) (bool, int, error) {
+	exactlyOneDifferent := false
+	indexOfDif := 0
+
+	// I think all the strings are the same length
+	if len(a) != len(b) {
+		return false, 0, fmt.Errorf("day02 compareIds(%s, %s) received unexpected input. Expected a and b to be the same length", string(a), string(b))
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] == b[i] {
+			continue
+		}
+
+		if exactlyOneDifferent {
+			return false, 0, nil
+		}
+
+		exactlyOneDifferent = true
+		indexOfDif = i
+	}
+
+	return exactlyOneDifferent, indexOfDif, nil
+}
+
+// there's probably a more efficient way to do this
+func findCommonLetters(f io.ReadSeeker) ([]rune, error) {
+	var ids [][]rune
+	var commonLetters []rune
+	s := bufio.NewScanner(f)
+
+ScanIDs:
+	for s.Scan() {
+		currID := []rune(s.Text())
+
+		for _, id := range ids {
+			// this is super dumb, must be a better way
+			if len(id) == 0 {
+				continue
+			}
+
+			isPair, indexOfDif, err := compareIds(id, currID)
+			if err != nil {
+				return commonLetters, err
+			}
+
+			if isPair {
+				commonLetters = append(
+					currID[:indexOfDif],
+					currID[indexOfDif+1:]...,
+				)
+
+				break ScanIDs
+			}
+		}
+
+		ids = append(ids, currID)
+	}
+
+	if s.Err() != nil {
+		return commonLetters, s.Err()
+	}
+
+	return commonLetters, nil
+}
+
 func runDay02(f io.ReadSeeker) {
 	checksum, err := checksum(f)
 	if err != nil {
 		log.Fatalf("day02 checksum: Encountered unexpected error %v", err)
 	}
 
-	fmt.Printf("%d\n", checksum)
+	f.Seek(0, 0)
+	commonLetters, err := findCommonLetters(f)
+	if err != nil {
+		log.Fatalf("day02 findCommonLetters: Encountered unexpected error %v", err)
+	}
+	fmt.Printf("part1: %d\n", checksum)
+	fmt.Printf("part2: %s\n", string(commonLetters))
 }
--- day02_test.go
+++ day02_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -52,6 +53,59 @@ func TestDay02ChecksumPartial(t *testing.T) {
 		twice, thrice := checksumPartial(tc.Input)
 		if twice != tc.Expect[0] || thrice != tc.Expect[1] {
 			t.Errorf("day02 checksumPartial expected %v, %v got %v, %v", tc.Expect[0], tc.Expect[1], twice, thrice)
+		}
+	}
+}
+
+func TestDay02CompareIds(t *testing.T) {
+	tests := []struct {
+		i1 []rune
+		i2 []rune
+		e1 bool
+		e2 int
+	}{
+		{[]rune("abcde"), []rune("axcye"), false, 0},
+		{[]rune("fghij"), []rune("fguij"), true, 2},
+	}
+
+	for _, tc := range tests {
+		isPair, i, err := compareIds(tc.i1, tc.i2)
+		if err != nil {
+			t.Fatalf("day02 compareIds received unexpected error: %v", err)
+		}
+
+		if isPair != tc.e1 || i != tc.e2 {
+			t.Errorf("day02 compareIds expected %v, %v got %v, %v", tc.e1, tc.e2, isPair, i)
+		}
+	}
+}
+
+func TestDay02FindCommonLetters(t *testing.T) {
+	tests := []struct {
+		Input  string
+		Expect []rune
+	}{
+		{`
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+`, []rune("fgij"),
+		},
+	}
+
+	for _, tc := range tests {
+
+		commonLetters, err := findCommonLetters(strings.NewReader(tc.Input))
+		if err != nil {
+			t.Fatalf("day02 findCommonLetters received unexpected error: %v", err)
+		}
+
+		if !reflect.DeepEqual(tc.Expect, commonLetters) {
+			t.Errorf("day02 findCommonLetters expected %s, got %s", string(tc.Expect), string(commonLetters))
 		}
 	}
 }
